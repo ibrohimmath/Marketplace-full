2af9dee666a2adcf2cc8f6939c2892ebf647286a
ibrohimmath <ahmadjonovibrohim062@gmail.com>
2024-06-15 16:15:21 +0500

    added Creators component inside creators branch

--- src/components/Card/type/CardCollection.jsx
+++ src/components/Card/type/CardCollection.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import clsx from "clsx";
+
 import { ImgSquare } from "../../Img";
 import ImgBox from "../components/ImgBox";
 import Content from "../components/Content";
